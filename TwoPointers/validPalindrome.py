a8987e478e6ca5f731c47815df5dab575426588e
Corentin <corentin.damas@gmail.com>
2023-07-20 10:28:50 +0200

    valid Palindrome Solution 2

--- TwoPointers/validPalindrome.py
+++ TwoPointers/validPalindrome.py
@@ -3,8 +3,9 @@
 # Alphanumeric characters include letters and numbers.
 
 # Given a string s, return true if it is a palindrome, or false otherwise.
+s = "A man, a plan, a canal: Panama"
 
-s = " "
+# Solution 1
 def isPalindrome(s:str)->bool:
     s = s.lower()
     # Check all char one by on and add it to a new string if it's Alpha-numemric  
@@ -12,7 +13,31 @@ def isPalindrome(s:str)->bool:
     # cleanS[::-1] create a reverse list. Will take all list [::] but starting from -1 (end of list)
     if cleanS == cleanS[::-1]: return True
     else: return False
-
-
-print(isPalindrome(s))
-        
+    
+#Solution 2   
+def isPalindrome2(s:str)->bool:
+    """_summary_
+    This version is require less storage space from solution 1
+    Where we had to add to the memory few extra lists. 
+    But Solution 2 is slower that Solution 1
+    """
+    l,r = 0, len(s) - 1
+    while l < r :
+        while l < r and not alphaNum(s[l]):
+            l += 1 
+        while r > l and not alphaNum(s[r]):
+            r -= 1 
+        if s[l].lower() != s[r].lower():
+            print(s[l], s[r])
+            return False
+        l += 1 
+        r -= 1       
+    return True
+    
+def alphaNum(c):
+    # return True if c charactere is inbetween A-Z or a-z or 0-9
+    return (ord('A')<= ord(c)<= ord('Z') or
+            ord('a')<= ord(c)<= ord('z') or
+            ord('0')<= ord(c)<= ord('9'))
+    
+print(isPalindrome(s), isPalindrome2(s))
