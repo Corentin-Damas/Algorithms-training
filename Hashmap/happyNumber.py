71cfdcc51417b5032aba0f9adb61eaac2c28202c
Nazemrap <corentin.damas@gmail.com>
2024-04-11 08:41:47 +0200

    #202. Happy Number - Done

---
+++ Hashmap/happyNumber.py
@@ -0,0 +1,28 @@
+# A happy number is a number defined by the following process:
+
+# Starting with any positive integer, replace the number by the sum of the squares of its digits.
+# Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+# Those numbers for which this process ends in 1 are happy.
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        visited = set()
+
+        while n not in visited:
+            visited.add(n)
+            n = self.sumOfSquares(n)
+
+            if n == 1:
+                return True
+
+        return False
+
+    def sumOfSquares(self, n: int) -> int:
+        out = 0
+        while n:
+            digit = n % 10
+            digit = digit ** 2
+            out += digit
+            n = n//10
+        return out
