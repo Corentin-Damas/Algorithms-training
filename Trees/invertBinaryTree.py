f1df0df202484609de51132e6142f90f47d35ddb
Nazemrap <corentin.damas@gmail.com>
2024-05-16 07:53:48 +0200

    #226. Invert Binary Tree - Done

--- Trees/invertBinaryTree.py
+++ Trees/invertBinaryTree.py
@@ -1,23 +1,47 @@
+from typing import Optional
+
 # Definition for a binary tree node.
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+
 def invertTree(root: TreeNode) -> TreeNode:
     # Recursive definition, invert the 2 child of a node a do it for all nodes
-    
-    # Base case, no childrens 
+
+    # Base case, no childrens
     if not root:
         return None
-    
+
     # swap the children
     tmp = root.left
     root.left = root.right
     root.right = tmp
-    
-    invertTree(root.left) # recursion will do the same as above for all and return when no root
-    invertTree(root.right) 
-    
-    return root # when finished
+
+    # recursion will do the same as above for all and return when no root
+    invertTree(root.left)
+    invertTree(root.right)
+
+    return root  # when finished
+
+
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
