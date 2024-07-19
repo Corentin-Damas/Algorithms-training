fa98df82d6ba6ef7e8105c5a0cb20a29504baf53
Corentin <corentin.damas@gmail.com>
2024-07-19 07:35:07 +0200

    #373. Find K Pairs with Smallest Sums

--- heap/IPO.py
+++ heap/IPO.py
@@ -1,5 +1,6 @@
 # 502. IPO
 from typing import List
+import heapq
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
         maxPRofit = []
