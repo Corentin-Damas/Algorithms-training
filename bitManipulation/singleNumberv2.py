e854affb4c51cbdfc0df09b59fb157b821281e1e
Corentin <corentin.damas@gmail.com>
2024-07-26 07:40:51 +0200

    #137. Single Number II

---
+++ bitManipulation/singleNumberv2.py
@@ -0,0 +1,13 @@
+#137. Single Number II
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        one, two = 0,0
+        for x in nums:
+            two = two ^ (one & x)
+            one = one ^ x
+            not_three = ~(two & one)
+            
+            one, two = not_three & one , not_three & two
+        return one
