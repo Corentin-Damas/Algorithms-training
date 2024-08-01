2d731a58747727f807b55d3e455d74c730aa780e
Corentin <corentin.damas@gmail.com>
2024-08-01 08:01:25 +0200

    172. Factorial Trailing Zeroes

---
+++ Math/factoTrailingZeros.py
@@ -0,0 +1,9 @@
+#172. Factorial Trailing Zeroes
+
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        count = 0
+        while n > 0:
+            n //= 5
+            count += n
+        return count
